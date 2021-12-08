fdbaa84dd477282cf932f3f2c17822a832393117
Aaron Minnick <abminnick@gmail.com>
2021-12-08 15:13:42 -0800

    pass test for draws
    Co-authored-by: Christy Welch <christinamawel@gmail.com>
    Co-authored-by: Curtis Brooks <curtisbrooks678@gmail.com>

--- RockPaperScissors/Models/Throw.cs
+++ RockPaperScissors/Models/Throw.cs
@@ -8,9 +8,63 @@ namespace RockPaperScissors.Models
 
     }
 
-    public static void ChooseWinner(string playerOneMove, string playerTwoMove)
+    public static string ChooseWinner(string playerOneMove, string playerTwoMove)
     {
-
+      string result = "";
+      if (playerOneMove == playerTwoMove) 
+      {
+        result = "Draw!";
+      }
+      else if (playerOneMove == "rock") 
+      {
+        if (playerTwoMove == "paper")
+        {
+          result = "Player 2 wins with paper!";
+        }
+        else if (playerTwoMove == "scissors")
+        {
+          result = "Player 1 wins with rock!";
+        }
+        else
+        {
+          result = "Bad input for player 2. rock, paper, or scissors expected.";
+        }
+      }
+      else if (playerOneMove == "paper")
+      {
+        if (playerTwoMove == "rock") 
+        {
+          result = "Player 1 wins with paper!";
+        }
+        else if (playerTwoMove == "scissors") 
+        {
+          result = "Player 2 wins with scissors!";
+        }
+        else
+        {
+          result = "Bad input for player 2. rock, paper, or scissors expected.";
+        }
+      }
+      else if (playerOneMove == "scissors")
+      {
+        if (playerTwoMove == "paper")
+        {
+          result = "Player 1 wins with scissors!";
+        }
+        else if (playerTwoMove == "rock")
+        {
+          result = "Player 2 wins with rock!";
+        }
+        else
+        {
+          result = "Bad input for player 2. rock, paper, or scissors expected.";
+        }
+      }
+      else
+      {
+        result = "Bad input for player 1. rock, paper, or scissors expected.";
+      }
+      return result;
     }
   }
 }
