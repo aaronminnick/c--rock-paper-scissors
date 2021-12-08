3f9ec8d64cfb4ae33eb251ff9a1b327bd6486bb7
Aaron Minnick <abminnick@gmail.com>
2021-12-08 14:59:24 -0800

    add tests for various win conditions
    Co-authored-by: Christy Welch <christinamawel@gmail.com>
    Co-authored-by: Curtis Brooks <curtisbrooks678@gmail.com>

--- RockPaperScissors.Tests/ModelTests/ThrowTests.cs
+++ RockPaperScissors.Tests/ModelTests/ThrowTests.cs
@@ -8,5 +8,39 @@ namespace RockPaperScissors.Tests
   public class ThrowTests
   {
     [TestMethod]
+    public void ChooseWinner_P1RockBeatsP2Scissors_String()
+    {
+      Assert.AreEqual("Player 1 wins with rock!", Throw.ChooseWinner("rock", "scissors"));
+    }
+
+    [TestMethod]
+    public void ChooseWinner_P1PaperBeatsP2Rock_String()
+    {
+      Assert.AreEqual("Player 1 wins with paper!", Throw.ChooseWinner("paper", "rock"));
+    }
+
+    [TestMethod]
+    public void ChooseWinner_P1ScissorsBeatsP2Paper_String()
+    {
+      Assert.AreEqual("Player 1 wins with scissors!", Throw.ChooseWinner("scissors", "paper"));
+    }
+
+    [TestMethod]
+    public void ChooseWinner_P2RockBeatsP1Scissors_String()
+    {
+      Assert.AreEqual("Player 2 wins with rock!", Throw.ChooseWinner("scissors", "rock"));
+    }
+
+    [TestMethod]
+    public void ChooseWinner_P2PaperBeatsP1Rock_String()
+    {
+      Assert.AreEqual("Player 2 wins with paper!", Throw.ChooseWinner("rock", "paper"));
+    }
+
+    [TestMethod]
+    public void ChooseWinner_P2ScissorsBeatsP1Paper_String()
+    {
+      Assert.AreEqual("Player 2 wins with scissors!", Throw.ChooseWinner("paper", "scissors"));
+    }
   }
 }
