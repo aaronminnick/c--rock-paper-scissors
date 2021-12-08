5a8b438a6c133d92db323627d4fd22a9e1bb1e65
Aaron Minnick <abminnick@gmail.com>
2021-12-08 14:50:12 -0800

    add boilerplate
    Co-authored-by: Christy Welch <christinamawel@gmail.com>
    Co-authored-by: Curtis Brooks <curtisbrooks678@gmail.com>

---
+++ RockPaperScissors.Tests/ModelTests/ThrowTests.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using RockPaperScissors.Models;
+
+namespace RockPaperScissors.Tests
+{
+  [TestClass]
+  public class ThrowTests
+  {
+    [TestMethod]
+  }
+}
---
+++ RockPaperScissors/Models/Throw.cs
@@ -0,0 +1,16 @@
+
+namespace RockPaperScissors.Models
+{
+  public class Throw
+  {
+    public Throw()
+    {
+
+    }
+
+    public static void ChooseWinner(string playerOneMove, string playerTwoMove)
+    {
+
+    }
+  }
+}
